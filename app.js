2729a22e408561523dba6e19ea5687a46f3d343d
luchiangel <luchinico2010@gmail.com>
2023-07-25 20:51:27 -0300

    holas

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
   })
 
-app.get("/",function(req , ques){
+app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
 
-app.get("/register",function(req , ques){
+app.get("/register",function(req,res){
     res.sendFile(path.join(__dirname,"/views/register.html"))
 })
 
-app.get("/login",function(req , ques){
+app.get("/login",function(req ,res){
     res.sendFile(path.join(__dirname,"/views/login.html"))
 })
